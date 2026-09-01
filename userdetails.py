13309ecc90bb2a64771ec5f592d63810e36d0753
shashank <shashankkuri12345@gmail.com>
2026-09-01 09:53:38 +0530

    saving the file

---
+++ userdetails.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your firstname: ")
+lastName = input("Enter your lastname: ")
+city = input("Enter your city: ")
+
+print("Your details:", firstName + " " + lastName + " " + city)
